395b801eb6feb04a0c84bf2f3f7824414e7eabe4
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-02-04 18:02:53 -0600

    routing pages

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+// maybe larger object or constructor
+
+var friendsArray = [
+  {
+    friendName: "Ahmed",
+    customerEmail: "ahmed@example.com",
+    customerID: "afhaque89",
+    phoneNumber: "000-000-0000"
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,21 @@
+var friendsData = require("../data/friends.js");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  // app.post("/api/friends", function(req, res) {
+  //   // update friends
+  //   if (friendsData.length < 5) {
+  //     tableData.push(req.body);
+  //     res.json(true);
+  //   }
+  //   else {
+  //     waitListData.push(req.body);
+  //     res.json(false);
+  //   }
+  // });
+
+}
