fb636ceed7a8af51a3ae2151f49241aa44d65731
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-02-09 00:46:30 -0600

    making progress

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,44 +1,54 @@
+
 var friendsData = require("../data/friends.js");
+var friend;
 
 module.exports = function(app) {
 
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
-    //  console.log(friendsData)
+    // console.log(friendsData)
   });
 
   app.post("/api/friends", function(req, res) {
     // update friends
-    // if (friendsData.length < 5) {
 
     var scoring = req.body.scores.map(a=>parseFloat(a));
-    // if (friendsData) {
+
       var newFriend = {
         name: req.body.name,
         photo: req.body.photo,
         scores: scoring
       }
-      console.log(newFriend)
-      // var name = req.body.name;
-      // var photo = req.body.photo;
-      // var scoring = req.body.scores.map(a=>parseFloat(a));
-      // console.log(scoring)
 
-      // friendsData.push(req.body);
+      console.log(newFriend);
       friendsData.push(newFriend);
-      // console.log(req.body)
-      // console.log(req.body.name)
-      // console.log(req.body.photo)
-      // console.log(req.body.parseFloat(scores));
-      // console.log("here is the name: " + req.body.name)
-      // console.log("Here are the scores: " + req.body.scores)
       
-
       var possibleMatches = friendsData.filter(match => match.name != newFriend.name)
-      console.log(possibleMatches)
+      // console.log(possibleMatches);
+      
+      var scoreCountArray = [];
+      possibleMatches.forEach(function (match){
+        var total = 0;
+        for (i=0; i<match.scores.length; i++) {
+          // var tempArray = []
+          total += Math.abs(match.scores[i] - newFriend.scores[i]);
+        }
+        scoreCountArray.push(total);
+      })
 
-      // res.json(true);
-      res.json(possibleMatches);
+      console.log("score board says: " + scoreCountArray)
+
+      // for (i=0; i<possibleMatches.length; i++) {
+      //   var diffArray = [];
+      //   console.log(possibleMatches[i]);
+      //   var diff = Math.abs(possibleMatches[i].scores[i] - newFriend.scores[i])
+      //   scoreCountArray.push[diff]
+      // }
+      
+      // console.log(possibleMatches)
+      res.json(scoreCountArray);
+      // res.json(postMessage);
+
   });
 
 }
